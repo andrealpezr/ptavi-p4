bbf60dacc07072116e692b21480bcef785416762
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-11-01 18:56:16 +0100

    Retoques, antes daba error

--- client.py
+++ client.py
@@ -18,16 +18,16 @@ PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
 USER = sys.argv[4]
-EXPIRES = int(sys.argv[5])
+EXPIRES = sys.argv[5]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", USER)
-    if sys.argv[3] == 'register':
-        my_socket.send(bytes('REGISTER sip:' + USER + ' SIP/2.0\r\nExpires: ' +
-                             EXPIRES + '\r\n\r\n', 'utf-8') + b'\r\n')
+    if sys.argv[3] == 'register': 
+        mensaje = ('REGISTER sip:' + USER + ' SIP/2.0\r\n')
+        mensaje += ('EXPIRES: ' + EXPIRES + '\r\n\r\n')
+        print(mensaje)
+        my_socket.send(bytes(mensaje, 'utf-8'))
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
-
-print("Socket terminado.")
+    print("Socket terminado.")
