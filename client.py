78b9348a55c8b3fc9473ddb4017cd23e3e121214
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-11-01 11:56:42 +0100

    Cliente con posibilidad de darse la baja
    Falta Pep8 y docstrings

--- client.py
+++ client.py
@@ -8,16 +8,25 @@ import socket
 import sys
 
 
-# Constantes. Dirección IP del servidor y contenido a enviar
+if len(sys.argv) < 6:
+    sys.exit('Usage: python3 client.py ip puerto '
+             'register sip_address expires_value')
+
+# Constantes. Dirección IP del servidor:
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = sys.argv[4]
+
+# Contenido que vamos a enviar
+USER = sys.argv[4]
+EXPIRES = int(sys.argv[5])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", USER)
+    if sys.argv[3] == 'register': 
+        my_socket.send(bytes('REGISTER sip:' + USER + ' SIP/2.0\r\nExpires: '+  
+                             EXPIRES +'\r\n\r\n', 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
