c358c5446480aee39a1e7ca75c0dce93f7eba3fb
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-11-01 19:03:29 +0100

    Hasta Expires (punto 7)

--- server.py
+++ server.py
@@ -1,31 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
+import time
 import socketserver
 
-
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
-    def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion")
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-
+    misdatos = {}
+     
+    def time_out(self):
+        cliente = []
+        for client in self.misdatos:
+            time_expires = self.misdatos[client]['expires']
+            time_expire = time.strptime(time_expires, '%Y-%m-%d %H:%M:%S')
+            if time_expire <= time.gmtime(time.time()):
+                cliente.append(client)
+        for hora in cliente:
+            del self.misdatos[hora]
+                 
+    def handle(self):        
+        datos = self.rfile.read().decode('utf-8').split()
+        if datos[0] == 'REGISTER':
+            self.hora = float(time.time()) + float(datos[-1])            
+            self.expires = time.strftime('%Y-%m-%d %H:%M:%S', 
+                                         time.gmtime(self.hora))
+            self.datoscliente = {'address': self.client_address[0], 'expires': self.expires}
+            self.misdatos[datos[1].split(':')[-1]] = self.datoscliente
+            if int(datos[-1]) == 0:
+                del self.misdatos[datos[1].split(':')[-1]]     
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        self.time_out()
+        print (self.misdatos)
+        
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
-    # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
-
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
